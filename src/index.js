fc917eb4ca3e0eee83be7b2cc390cca11f12d2c5
Avi Haiat <thaiat@ipelia.fr>
2015-02-03 10:12:37 +0200

    Add yoobic-angular-core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 'use strict';
 var angular = require('angular');
-var app = angular.module('app', []);
-app.controller('home', function($scope) {
-    $scope.message = 'Welcome';
-});
+var YoobicCore = require('yoobic-angular-core')();
+
+var app = angular.module('app', [YoobicCore.name]);
+app.controller('home', [
+    '$scope',
+    function($scope) {
+        $scope.message = 'Welcome ';
+    }
+]);
